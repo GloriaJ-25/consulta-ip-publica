e9c86e1c95ad5d50fd21a2ded0d839e1ef8e3559
GloriaJ-25 <a3522110026@alumno.uttrehuacan.edu.mx>
2025-07-16 02:59:29 +0000

    Eliminé todas las carpetas y archivos innecesarios, limpieza final del proyecto

--- if-vlan.py
+++
@@ -1,9 +0,0 @@
-nativeVLAN = 100
-dataVLAN = 100
-
-if nativeVLAN == dataVLAN:
-    print("La VLAN nativa y los datos VLAN son iguales.")
-
-else:
-    
-    print("La VLAN nativa y los datos VLAN son diferentes.")  
